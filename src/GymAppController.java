806b07f21abab377ed950015bbabf191c171f938
Luis <luisgmgomes@gmail.com>
2022-11-14 00:44:58 -0300

    building

---
+++ src/GymAppController.java
@@ -0,0 +1,55 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GymAppController
+{
+
+    @FXML
+    void casdastraClien(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void casdastraFunci(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void excluir(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void listClient(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void listFunc(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void listTreino(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void sair(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void unicClienFunc(ActionEvent event) 
+    {
+
+    }
+
+}
